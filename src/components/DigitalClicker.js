958993f6403911ac94f8d6c99500c41610e3de95
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-19 13:56:06 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+import React from "react";
+
+export default class DigitalClicker extends React.Component {
+
+  state = {
+    timesClicked: 0
+  }
+
+  render() { 
+    return <button onClick={this.click}>{this.JanMichaelScott()}</button>;
+  }
+
+  JanMichaelScott() {
+    return this.state.timesClicked;
+  }
+
+  click = () => {
+    this.setState((previousState) => {
+      return {
+        timesClicked: previousState.timesClicked + 1 
+      }
+    })
+  }
+
+}
